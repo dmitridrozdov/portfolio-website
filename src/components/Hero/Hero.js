00b6f74567d0dd10f02095a19e7938232269880e
dmitridrozdov <dm.drozdov@gmail.com>
2021-06-30 15:41:34 +1000

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main cener>
+        Welcome to <br/>
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Building different react applications
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
